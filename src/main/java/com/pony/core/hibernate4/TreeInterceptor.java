828170306660f42f214e87e83bd8fb2a0b6e1776
wenbing1123 <wenbing1123@163.com>
2014-12-28 17:18:01 +0800

    domain finished

---
+++ src/main/java/com/pony/core/hibernate4/TreeInterceptor.java
@@ -0,0 +1,9 @@
+package com.pony.core.hibernate4;
+
+import org.hibernate.EmptyInterceptor;
+
+public class TreeInterceptor extends EmptyInterceptor {
+
+	private static final long serialVersionUID = 1L;
+
+}
