0800037bdf1f9f6aeddc33057d67acaa66007217
wenbing1123 <wenbing1123@163.com>
2015-01-06 15:39:24 +0800

    activiti alpha

--- src/main/java/com/pony/core/facade/impl/ActivitiFacadeImpl.java
+++ src/main/java/com/pony/core/facade/impl/ActivitiFacadeImpl.java
@@ -4,6 +4,7 @@ import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.zip.ZipInputStream;
@@ -21,9 +22,12 @@ import org.activiti.engine.TaskService;
 import org.activiti.engine.impl.RepositoryServiceImpl;
 import org.activiti.engine.impl.persistence.entity.ExecutionEntity;
 import org.activiti.engine.impl.persistence.entity.ProcessDefinitionEntity;
+import org.activiti.engine.impl.persistence.entity.TaskEntity;
 import org.activiti.engine.impl.pvm.PvmActivity;
 import org.activiti.engine.impl.pvm.PvmTransition;
 import org.activiti.engine.impl.pvm.process.ActivityImpl;
+import org.activiti.engine.impl.pvm.process.ProcessDefinitionImpl;
+import org.activiti.engine.impl.pvm.process.TransitionImpl;
 import org.activiti.engine.repository.Deployment;
 import org.activiti.engine.repository.ProcessDefinition;
 import org.activiti.engine.repository.ProcessDefinitionQuery;
@@ -31,9 +35,12 @@ import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.DelegationState;
 import org.activiti.engine.task.Task;
 import org.activiti.image.impl.DefaultProcessDiagramGenerator;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
+import com.pony.core.SystemException;
 import com.pony.core.facade.ActivitiFacade;
+
 
 /**
  * 
@@ -44,6 +51,9 @@ import com.pony.core.facade.ActivitiFacade;
 @Service("activitiFacade")
 @Transactional
 public class ActivitiFacadeImpl implements ActivitiFacade{
+	
+	public final static String ACTIVITY_ID_START = "START";
+	public final static String ACTIVITY_ID_END = "END";
 
 	@Resource private RepositoryService repositoryService;
 	@Resource private RuntimeService runtimeService;
@@ -317,5 +327,134 @@ public class ActivitiFacadeImpl implements ActivitiFacade{
 	public Object[] backStep(String instanceId, String userId) {
 		return null;
 	}
-
+	
+	//任务转办操作
+	public void transferAssignee(String taskId, String userId) {  
+        taskService.setAssignee(taskId, userId);  
+    } 
+	
+	
+	//========================>private operation
+	private ProcessDefinitionEntity findProcessDefinitionEntityByTaskId(String taskId){
+		return findProcessDefinitionEntityById(findTaskEntityById(taskId).getProcessDefinitionId());
+	}
+	private ExecutionEntity findExecutionEntityByInstanceId(String instanceId){
+		ExecutionEntity execution = (ExecutionEntity)runtimeService.createProcessInstanceQuery().processInstanceId(instanceId).singleResult();
+		if(execution == null){
+			throw new SystemException("Execution not Found");
+		}
+		return execution;
+	}
+	private TaskEntity findTaskEntityById(String taskId){
+		TaskEntity task = (TaskEntity) taskService.createTaskQuery().taskId(taskId).singleResult();
+		if(task == null){
+			throw new SystemException("Task not Found");
+		}
+		return task;
+	}
+	private ProcessDefinitionEntity findProcessDefinitionEntityById(String definitionId){
+		ProcessDefinitionEntity definition =(ProcessDefinitionEntity) ((RepositoryServiceImpl)repositoryService).getDeployedProcessDefinition(definitionId);
+		if(definition == null){
+			throw new SystemException("ProcessDefinition not Found");
+		}
+		return definition;
+	}
+	/**
+	 * 根据任务ID和节点ID获取活动节点
+	 * @param taskId
+	 * @param activityId
+	 * @return
+	 */
+	private ActivityImpl findActivitiImpl(String taskId, String activityId){
+		//获取流程定义
+		ProcessDefinitionEntity processDefinition = findProcessDefinitionEntityByTaskId(taskId);  
+		// 获取当前活动节点ID
+		if (StringUtils.isBlank(activityId)) { 
+			activityId = findTaskEntityById(taskId).getTaskDefinitionKey();
+		}
+		// 根据流程定义，获取该流程实例的结束节点
+		if (activityId.toUpperCase().equals(ACTIVITY_ID_END)) {
+			for (ActivityImpl activityImpl : processDefinition.getActivities()) {
+				List<PvmTransition> pvmTransitionList = activityImpl.getOutgoingTransitions();
+				if (pvmTransitionList.isEmpty()) {  
+                    return activityImpl;  
+                }  
+			}
+		}
+		ActivityImpl activityImpl = ((ProcessDefinitionImpl) processDefinition).findActivity(activityId);
+		return activityImpl;
+	}
+	/**
+	 * 提交流程操作，即下一步
+	 * 
+	 * @param taskId
+	 * @param variables
+	 * @param activityId
+	 */
+	private void commitProcess(String taskId, Map<String, Object> variables,  String activityId){
+		if (variables == null) {  
+            variables = new HashMap<String, Object>();  
+        } 
+		//活动为空，即没有手工指定流向活动，执行默认流向，有条件根据条件流向，无条件默认第一个流向
+		if (StringUtils.isBlank(activityId)) { 
+			taskService.complete(taskId, variables);
+		}else{
+			turnTransition(taskId,activityId, variables);
+		}
+	}
+	/**
+	 * 手工指定流程转向操作
+	 * 
+	 * @param taskId
+	 * @param activityId
+	 * @param variables
+	 */
+	private void turnTransition(String taskId, String activityId, Map<String, Object> variables){
+		// 当前节点  
+        ActivityImpl currActivity = findActivitiImpl(taskId, null);
+        // 清空当前流向  
+        List<PvmTransition> oriPvmTransitionList = clearTransition(currActivity);
+        // 创建新流向  
+        TransitionImpl newTransition = currActivity.createOutgoingTransition();
+        // 目标节点  
+        ActivityImpl pointActivity = findActivitiImpl(taskId, activityId);
+       // 设置新流向的目标节点  
+       newTransition.setDestination(pointActivity); 
+       // 执行转向任务  
+       taskService.complete(taskId, variables);
+       // 删除目标节点新流入  
+       pointActivity.getIncomingTransitions().remove(newTransition);
+       // 还原以前流向  
+       restoreTransition(currActivity, oriPvmTransitionList);
+	}
+	/**
+	 * 清空指定活动节点流向 
+	 * 
+	 * @param activityImpl
+	 * @return 节点流向集合
+	 */
+	private List<PvmTransition> clearTransition(ActivityImpl activityImpl) { 
+		List<PvmTransition> oriPvmTransitionList = new ArrayList<PvmTransition>();  
+		// 获取当前节点所有流向，存储到临时变量，然后清空  
+        List<PvmTransition> pvmTransitionList = activityImpl.getOutgoingTransitions();
+        for (PvmTransition pvmTransition : pvmTransitionList) {  
+            oriPvmTransitionList.add(pvmTransition);  
+        }
+        pvmTransitionList.clear();
+        return oriPvmTransitionList;
+	}
+	/**
+	 * 还原指定活动节点流向 
+	 * @param activityImpl
+	 * @param oriPvmTransitionList
+	 */
+	private void restoreTransition(ActivityImpl activityImpl,  List<PvmTransition> oriPvmTransitionList) { 
+		// 清空现有流向  
+        List<PvmTransition> pvmTransitionList = activityImpl .getOutgoingTransitions();
+        pvmTransitionList.clear();
+        // 还原以前流向  
+        for (PvmTransition pvmTransition : oriPvmTransitionList) {  
+            pvmTransitionList.add(pvmTransition);  
+        }  
+	}
 }
